da3a6e0fd2408fe718914218fb5010e24919df0a
AlexvonKarstedt <80624199+AlexvonKarstedt@users.noreply.github.com>
2021-05-07 17:46:13 -0400

    removed react loader spinner

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Loader from "react-loader-spinner";
+
 
 export default function WeatherSearch() {
   const [city, setCity] = useState(null);
@@ -53,11 +53,5 @@ export default function WeatherSearch() {
     );
   } else {
     return form;
-    <Loader
-        type="ThreeDots"
-        color="purple"
-        height={100}
-        width={100}
-      />
   }
 }
